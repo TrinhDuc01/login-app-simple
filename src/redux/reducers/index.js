3e24ab53e6a22acf279a27fea99d3f8469f53131
TrinhDuc01 <ductrinh.0301@gmail.com>
2024-11-18 09:17:07 +0700

    combine Reducer

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import userReducer from "./userReducer";
+import themeReducer from "./themeReducer";
+export default combineReducers({
+    userReducer,
+    themeReducer
+})
