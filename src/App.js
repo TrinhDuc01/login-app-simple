6cd8a08f0f948ab006519f60e4762a75dd14f3f5
TrinhDuc01 <ductrinh.0301@gmail.com>
2024-11-18 09:16:23 +0700

    add toastify, theme, login component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+
+import { useSelector } from 'react-redux';
 import './App.css';
+import Login from './components/Login';
+import Theme from './components/Theme';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
+  const backgroundColor = useSelector(state => state.themeReducer.backgroundColor)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="App" style={{ backgroundColor: backgroundColor, transition: "3s" }}>
+      <Theme />
+      <Login />
+      <ToastContainer
+
+      />
+      {/* Same as */}
+      <ToastContainer />
     </div>
+
   );
 }
 
